2a3d8921450d0cd2ba73004bbeb93046da417f5d
wyz <1447979361@qq.com>
2020-10-31 21:53:07 +0800

    添加文件

---
+++ src/main/java/com/rjjs/test/test.java
@@ -0,0 +1,11 @@
+package com.rjjs.test;
+
+import org.junit.Test;
+
+public class test {
+	@Test
+	public void m() {
+		System.out.println(1);
+	}
+
+}
---
+++ src/test/java/com/rjjs/test/test1.java
@@ -0,0 +1,10 @@
+package com.rjjs.test;
+
+import org.junit.Test;
+
+public class test1 {
+	@Test
+	public void t() {
+		System.out.println(1);
+	}
+}
